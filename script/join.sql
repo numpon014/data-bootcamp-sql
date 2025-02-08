4d9e57865ac4a6ec14cc7f50053b563967b9ed7d
Numpon Maytangsuphut <numpon014@gmail.com>
2025-02-08 18:47:39 +0700

    Join - Basic

--- script/join.sql
+++
@@ -1,5 +0,0 @@
-SELECT *
-FROM artists as ar
-         JOIN albums as al
-              ON ar.ArtistId = al.ArtistId
-WHERE ar.name LIKE 'J%';
---
+++ script/join_basic.sql
@@ -0,0 +1,79 @@
+-- Join 2 Tables,
+-- Primary Key = Foreign Key
+SELECT *
+FROM artists as ar
+         JOIN albums as al
+              ON ar.ArtistId = al.ArtistId
+WHERE ar.name LIKE 'J%';
+
+
+-- Join by USING
+-- In this case, we don't need to specify the column name
+-- The column name is the same in both tables
+SELECT *
+FROM artists as ar
+         JOIN albums as al
+              USING (ArtistId)
+WHERE ar.name LIKE 'J%';
+
+
+-- JOIN Multiple Tables
+SELECT
+    ar.Name as artist_name,
+    al.Title as album_title,
+    tr.Name as track_name,
+    ge.Name as genre_name
+FROM artists as ar
+         JOIN albums as al
+              ON ar.ArtistId = al.ArtistId
+         JOIN tracks as tr
+              ON al.AlbumId = tr.AlbumId
+         JOIN genres as ge
+              ON tr.GenreId = ge.GenreId
+
+
+
+-- JOIN Multiple Tables with WHERE Clause
+SELECT
+    ar.Name as artist_name,
+    al.Title as album_title,
+    tr.Name as track_name,
+    ge.Name as genre_name
+FROM artists as ar
+         JOIN albums as al
+              ON ar.ArtistId = al.ArtistId
+         JOIN tracks as tr
+              ON al.AlbumId = tr.AlbumId
+         JOIN genres as ge
+              ON tr.GenreId = ge.GenreId
+WHERE ge.name in ('Jazz', 'Pop', 'Rock');
+
+-- JOIN Multiple Tables with WHERE Clause and Group By
+SELECT
+    ge.Name as genres_name,
+    COUNT(*) as total_tracks
+FROM artists as ar
+         JOIN albums as al
+              ON ar.ArtistId = al.ArtistId
+         JOIN tracks as tr
+              ON al.AlbumId = tr.AlbumId
+         JOIN genres as ge
+              ON tr.GenreId = ge.GenreId
+WHERE ge.name in ('Jazz', 'Pop', 'Rock')
+GROUP BY 1
+ORDER BY 2 DESC;
+
+-- JOIN Multiple Tables with WHERE Clause and Group By
+SELECT
+    ar.name as artist_name,
+    ge.Name as genres_name,
+    COUNT(*) as total_tracks
+FROM artists as ar
+         JOIN albums as al
+              ON ar.ArtistId = al.ArtistId
+         JOIN tracks as tr
+              ON al.AlbumId = tr.AlbumId
+         JOIN genres as ge
+              ON tr.GenreId = ge.GenreId
+WHERE ge.name in ('Jazz', 'Pop', 'Rock')
+GROUP BY 1,2
