a9e54f1c888b7bc0390d5092b5781ae7e5e56325
Numpon Maytangsuphut <numpon014@gmail.com>
2025-02-09 13:30:29 +0700

    group by, having, order by functions

--- on-demand/scripts/group_by.sql
+++ on-demand/scripts/group_by.sql
@@ -1,5 +1,14 @@
 -- Aggregate functions with GROUP BY clause
-SELECT genres.name, COUNT(*) AS count_songs
-FROM genres, tracks
+SELECT genres.name,
+       COUNT(*) AS count_songs
+FROM genres,
+     tracks
 WHERE genres.genreid = tracks.genreid
+GROUP BY genres.name;
+
+-- Aggregate functions with JOIN clause and GROUP BY clause
+SELECT genres.name,
+       COUNT(*) AS count_songs
+FROM genres
+         JOIN tracks ON genres.genreid = tracks.genreid
 GROUP BY genres.name;
